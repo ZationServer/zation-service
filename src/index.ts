dc76216e3b617e07c934d5fb23aa632a71af9da6
Luca Scaringella <lucalstyle@gmail.com>
2019-02-08 22:39:08 +0100

    implement service module api

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+©Copyright by Luca Scaringella
+ */
+
+import {ServiceModule} from "./lib/serviceModule";
+
+export {
+       ServiceModule
+    };
---
+++ src/lib/serviceModule.ts
@@ -0,0 +1,27 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+©Copyright by Luca Scaringella
+ */
+
+export type CustomServiceCreateFunction<T,C> = (config : C,name : string) => Promise<T> | T;
+export type CustomServiceGetFunction<T,R> = (service : T) => Promise<R> | R;
+
+export interface ServiceModule<Config,Created,Get> {
+   serviceName : string,
+   service : CustomService<Config,Created,Get> | Record<string,Config> | DefaultConfig<Config>;
+   bagExtensions : {
+       smallBagCompatible  : boolean;
+       methods : Record<string,Function>;
+   }
+}
+
+export interface CustomService<Config,Created,Get> {
+    create : CustomServiceCreateFunction<Created,Config>;
+    get ?: CustomServiceGetFunction<Created,Get>
+}
+
+export interface DefaultConfig<T> {
+    default ?: T;
+}
+
