a91852964a430beee71e4c80d297150842ccedbf
Luca Scaringella <lucalstyle@gmail.com>
2020-02-15 01:59:25 +0100

    Remove dependency to bag extensions. Improve the service module interface.

--- src/lib/serviceModule.ts
+++ src/lib/serviceModule.ts
@@ -4,34 +4,30 @@ GitHub: LucaCode
 ©Copyright by Luca Scaringella
  */
 
+export interface ServiceModule<Config,Created,Get> {
+    /**
+     * The Service Module.
+     * The keys are the name of the exported services.
+     * For example 'MySQL','NodeMailer','MongoDB'.
+     * The value is the service configuration.
+     * If you want to register bag extensions for a service,
+     * you should look at the 'zation-bag-extension' package.
+     * You also can check out the already created
+     * service modules: 'zation-service-nodemailer' or 'zation-service-mysql'.
+     */
+    [serviceName: string] : Service<Config,Config,Get>;
+}
+
 export type CustomServiceCreateFunction<T,C> = (config : C,name : string) => Promise<T> | T;
 export type CustomServiceGetFunction<T,R> = (service : T) => Promise<R> | R;
 
-export interface ServiceModule<Config,Created,Get,BagEx = Record<string,any>,RequestBagEx = Record<string,any>> {
-    /**
-     * The name of the service.
-     * @example 'MySQL','NodeMailer','MongoDB'
-     */
-   serviceName : string,
-    /**
-     * The configuration of the service same as the service property of the service configuration from the zation server.
-     */
-   service : CustomService<Config,Created,Get> | Record<string,Config> | DefaultConfig<Config>;
-    /**
-     * The bag extensions, same as one bag extension of the app configuration from the zation server.
-     */
-   bagExtensions : {
-       bag ?: BagEx;
-       requestBag ?: RequestBagEx;
-   }
+export interface Service<Config,Created,Get> {
+    create: CustomServiceCreateFunction<Created,Config>;
+    get?: CustomServiceGetFunction<Created,Get>
+    instances: DefaultInstance<Config> | Record<string,Config>;
 }
 
-export interface CustomService<Config,Created,Get> {
-    create : CustomServiceCreateFunction<Created,Config>;
-    get ?: CustomServiceGetFunction<Created,Get>
-}
-
-export interface DefaultConfig<T> {
+export interface DefaultInstance<T> {
     default ?: T;
 }
 
